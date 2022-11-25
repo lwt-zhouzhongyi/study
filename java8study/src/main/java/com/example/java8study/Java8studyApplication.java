13f551e31199dc692d4e3ad9791555bdb9384988
lwt-zhouzhongyi <17640414170@163.com>
2022-11-26 02:00:16 +0800

    :tada: first commit

---
+++ java8study/src/main/java/com/example/java8study/Java8studyApplication.java
@@ -0,0 +1,13 @@
+package com.example.java8study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8studyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8studyApplication.class, args);
+    }
+
+}
---
+++ java8study/src/test/java/com/example/java8study/Java8studyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8studyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
